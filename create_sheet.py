bc3b5e9c35ade55209f37cb5d49c4d12e1b5b7b9
Andy Fortman <andy@gobfink@gmail.com>
2020-08-21 00:38:41 -0400

    Now calculates min,max,avg,median,and stdev for each category

--- create_sheet.py
+++ create_sheet.py
@@ -1,5 +1,6 @@
 import csv
 from collections import defaultdict
+from statistics import stdev, mean, median
 
 filepath = "C:/Users/Andy/Desktop/git/bloodsugars/Blood_sugars.csv"
 
@@ -17,6 +18,7 @@ with open(filepath, newline='') as csvfile:
         
 date_sorted = {}
 date_strings = {}
+time_values = {}
 for date in date_dict:
     time_sorted = []
     for lower_bound,upper_bound in time_seperators:
@@ -34,7 +36,11 @@ for date in date_dict:
                 # if its empty print an empty spot
                 table_row += "|              |"
             else:
-                table_row += "|  %4d - %3d  |"%(int(time_sorted[t][row][0]),int(time_sorted[t][row][1]))
+                table_row += "|  %4d : %3d  |"%(int(time_sorted[t][row][0]),int(time_sorted[t][row][1]))
+                if t not in time_values.keys():
+                    time_values[t]=[int(time_sorted[t][row][1])]
+                else:
+                    time_values[t].append(int(time_sorted[t][row][1]))
             
         table_rows.append(table_row)
     #print (table_rows)
@@ -46,9 +52,19 @@ for date in date_dict:
 seperator_row ="======================================================================================================================================="
 date_seperator="---------------------------------------------------------------------------------------------------------------------------------------"    
 title_row="Date    | [%4d, %4d]"%(time_seperators[0][0],time_seperators[0][1])
+mean_row="Mean    |  %8f  |"%(mean(time_values[0]))
+median_row="Median  |  %3d         |"%(median(time_values[0]))
+stdev_row="Std.Dev.|  %8f  |"%(stdev(time_values[0]))
+min_row="Min     |  %3d         |"%(min(time_values[0]))
+max_row="Max     |  %3d         |"%(max(time_values[0]))
 
 for i in range(1,len(time_seperators)):
     title_row += " || " + str("[%4d, %4d]"%(time_seperators[i][0],time_seperators[i][1]))
+    mean_row += "| " + str(" %8f  |"%mean(time_values[i]))
+    median_row += "| " + str(" %3d         |"%median(time_values[i]))
+    stdev_row += "| " + str(" %8f   |"%stdev(time_values[i]))
+    min_row += "| " + str(" %3d         |"%min(time_values[i]))
+    max_row += "| " + str(" %3d         |"%max(time_values[i]))
 
 
 print(seperator_row)
@@ -62,4 +78,9 @@ for date in date_strings:
         print (date + date_strings[date][row])
     print (date_seperator)
 
+print (mean_row)
+print (median_row)
+print (stdev_row)
+print (min_row)
+print (max_row)
 print (seperator_row)
